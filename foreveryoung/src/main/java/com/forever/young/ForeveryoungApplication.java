08afed94470506fbefcbc7dd72e91dc8710c5fc0
BRAIN <BRAIN@DESKTOP-33B6BVF>
2021-03-25 19:43:06 +0900

    foreveryoung 첫 업로드

---
+++ foreveryoung/src/main/java/com/forever/young/ForeveryoungApplication.java
@@ -0,0 +1,13 @@
+package com.forever.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeveryoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeveryoungApplication.class, args);
+	}
+
+}
---
+++ foreveryoung/src/test/java/com/forever/young/ForeveryoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forever.young;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeveryoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
