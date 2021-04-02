5b768be5da2bc341bd68c07fc6f6307147356bf3
yujina25 <yjgood25@gmail.com>
2021-04-02 20:43:21 +0900

    상품 등록 이미지 파일 처리

---
+++ foreveryoung/src/main/java/com/forever/young/entity/ProductFileUpload.java
@@ -0,0 +1,58 @@
+package com.forever.young.entity;
+
+public class ProductFileUpload {
+	
+	private int image_no;
+	private int product_id;
+	private String image_name;
+	private String image_save_name;
+	private int file_size;
+	private String file_type;
+	
+	public ProductFileUpload() {
+		super();
+	}
+	
+	
+	public int getImage_no() {
+		return image_no;
+	}
+
+
+	public void setImage_no(int image_no) {
+		this.image_no = image_no;
+	}
+
+
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getImage_name() {
+		return image_name;
+	}
+	public void setImage_name(String image_name) {
+		this.image_name = image_name;
+	}
+	public String getImage_save_name() {
+		return image_save_name;
+	}
+	public void setImage_save_name(String image_save_name) {
+		this.image_save_name = image_save_name;
+	}
+	public int getFile_size() {
+		return file_size;
+	}
+	public void setFile_size(int file_size) {
+		this.file_size = file_size;
+	}
+	public String getFile_type() {
+		return file_type;
+	}
+	public void setFile_type(String file_type) {
+		this.file_type = file_type;
+	}
+
+}
