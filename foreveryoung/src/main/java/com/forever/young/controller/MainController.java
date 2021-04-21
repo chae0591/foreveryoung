cc340d80fa7f4f8ccbc5f8035bda19dae5aaab00
BRAIN <BRAIN@DESKTOP-33B6BVF>
2021-04-21 17:19:44 +0900

    main 페이지 product테이블 불러오기

--- foreveryoung/src/main/java/com/forever/young/controller/MainController.java
+++ foreveryoung/src/main/java/com/forever/young/controller/MainController.java
@@ -1,16 +1,36 @@
 
 package com.forever.young.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.forever.young.entity.Product;
+import com.forever.young.service.MainService;
+import com.forever.young.service.ProductService;
+
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 public class MainController {
 
-	@RequestMapping(value="/main")
-	public String main() {
-		return "main";
+	private final Logger log =
+			LoggerFactory.getLogger(MainController.class);
+	
+	@Autowired
+	private MainService service;
+	
+	
+	//메인화면(상품리스트 불러오기)
+	@GetMapping("/main")
+	public void productList(Model model) throws Exception{
+		log.info("main()");
+		
+		model.addAttribute("main", service.productList());
 	}
-
 
 }
